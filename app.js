340fc81e6a352534811f1ec4c468e5cc47e99dc3
Russell <russelldev@protonmail.com>
2021-01-19 08:24:01 +0000

    Add draw circle function

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let size = 20;
+let color = 'black';
+
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+drawCircle(100, 200);
